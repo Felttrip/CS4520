a81a24a1e394a7245154e89c74258ed649350088
Nate Thompson <nthompson55@gmail.com>
2014-09-04 08:51:42 -0500

    initial commit

---
+++ lab1/as1.c
@@ -0,0 +1,24 @@
+/***************************************************************************
+ * Name:           
+ * Userid:         
+ * Course:         CS 4520
+ * Assignment#:    Assignment 1
+ *
+ * File name:      as1.c
+ *
+ * Question no:	   2.26        
+ *
+ * Assumptions:    1)Input file name is lb1.input
+ *		&		   2)Output file name should be lb1.output
+ * Precautions	   3)Ensure that you have read and write permissions in the 
+ *				   directory 
+ * 				   4)
+ *				   5)
+ **************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
